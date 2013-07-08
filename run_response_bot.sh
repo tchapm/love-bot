f46c9676a3e5a8a8ae5b84ed660920209f5f427d
tchapm <tchap00@gmail.com>
2013-07-07 21:17:24 -0700

    modified to run on pi

--- run_response_bot.sh
+++ run_response_bot.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-JAR_PATH=/Users/tchap/.m2/repository/bot/LoveBot/0.1-SNAPSHOT/LoveBot-0.1-SNAPSHOT.jar
-java -cp $JAR_PATH com.bot.main.MainBotResponder $1;
+cd ~/git_repos/love-bot
+export JAR_PATH=~/.m2/repository/bot/LoveBot/0.1-SNAPSHOT/LoveBot-0.1-SNAPSHOT.jar
+export LOG_FILE=/home/pi/logs/bot_logs/$1`date +"%Y-%m-%d-%H-%M"`.log
+
+
+java -cp $JAR_PATH com.bot.main.MainBotResponder $1 >> $LOG_FILE 2>&1
 
